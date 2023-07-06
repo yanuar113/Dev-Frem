9754cd99b30a8c3ff97262ad407bc7f4d8353228
yanuar113 <yanuar.ardi113@gmail.com>
2023-07-06 16:07:04 +0700

    add tanggal

--- resources/views/rak_buku/form.blade.php
+++ resources/views/rak_buku/form.blade.php
@@ -1,6 +1,12 @@
 @extends('layout.app')
 @section('title', 'Daftar Rak Buku')
 @section('content')
+    <script> 
+        $(function () {
+            $("#datepicker").datepicker();
+            $(document).tooltip();
+        });
+    </script>
     <h2>{{ $store }} Data Rak Buku</h2>
     <form method="POST" action="{{ $action }}">
         @csrf
@@ -9,19 +15,21 @@
         @endif
         <input type="hidden" name="id" value="{{ $rak->id }}" />
         <div class="form-grup">
-        <input type="text" class="mail_text" name="nama" placeholder="Nama Rak" value="{{ $rak->nama }}" />
+        <input type="text" class="mail_text" name="nama" placeholder="Nama Rak" value="{{ $rak->nama }}" title="Masukan Nama Rak" />
         @error('nama')
         <b>{{$message}}</b>
         @enderror
         </div>
         <div class="form-grup">
-        <input type="text" class="mail_text" name="lokasi" placeholder="Lokasi" value="{{ $rak->lokasi }}" />
+        <input type="text" class="mail_text" name="lokasi" placeholder="Lokasi" value="{{ $rak->lokasi }}" title="Masukan Lokasi" />
         @error('lokasi')
         <b>{{$message}}</b>
         @enderror
         </div>
-        <input type="text" class="mail_text" name="keterangan" placeholder="keterangan" value="{{ $rak->keterangan }}" />
+        <input type="text" class="mail_text" name="keterangan" placeholder="keterangan" value="{{ $rak->keterangan }}" title="Masukan Keterengan"/>
+        <input type="text" class="mail_text" name="tanggal" placeholder="tanggal" id="datepicker" title="Masukan Tanggal"/>
         <input class="btn btn-success mt-3"type="submit" value="{{ $store }}" />
+
         
             <a class="btn btn-warning mt-3" href="{{ url('/rak_buku') }}">Kembali</a>
         
